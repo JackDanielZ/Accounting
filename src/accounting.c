9a808f466ab267fd27ace79b843a802ff6b74dc9
Daniel Zaoui <daniel.zaoui@yahoo.com>
2016-08-20 22:09:07 +0300

    Begin support of pdf generation
    The output is a sample.

--- src/accounting.c
+++ src/accounting.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <libgen.h>
+#include <getopt.h>
 
 #include <Elementary.h>
 
@@ -53,25 +54,100 @@ _my_win_del(void *data EINA_UNUSED, Evas_Object *obj EINA_UNUSED, void *event_in
    elm_exit(); /* exit the program's main loop that runs in elm_run() */
 }
 
+enum
+{
+   GEN_UI,
+   GEN_PDF
+};
+
+Eina_Bool
+_pdf_generate(Year_Desc *ydesc, const char *output)
+{
+   char cmd[128];
+   const char *tmp_file = "/tmp/accounting.tex";
+   FILE *fp = fopen(tmp_file, "w");
+
+   fprintf(fp,
+         "\\documentclass{article}\n"
+         "\\begin{document}\n"
+         "\\begin{table}[ht]\n"
+         "\\centering\n"
+         "\\begin{tabular}{c|ccccccc}\n"
+         "\\hline\n"
+         "& col1 & col2 & col3 & col4 & col5 & col6 & col7 \\\\\n"
+         "\\hline\n"
+         "row1& 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 \\\\\n"
+         "row2& 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 \\\\\n"
+         "row3& 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 \\\\\n"
+         "row4& 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 \\\\\n"
+         "row5& 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 \\\\\n"
+         "row6& 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 & 0.9 \\\\\n"
+         "\\hline\n"
+         "\\end{tabular}\n"
+         "\\end{table}\n"
+         "\\end{document}\n");
+   fclose(fp);
+
+   sprintf(cmd, "texi2pdf -q -c %s -o %s", tmp_file, output);
+   system(cmd);
+   return EINA_TRUE;
+}
+
 EAPI_MAIN int
 elm_main(int argc, char **argv)
 {
    char history_file[256];
    char *buffer, *dir;
-   int m;
+   int m, opt, gen_what = GEN_UI;
+   Eina_Bool help = EINA_FALSE;
    eina_init();
+
+   struct option opts[] = {
+      { "help",    no_argument,       NULL,       'h'        },
+      { "pdf",     no_argument,       &gen_what,  GEN_PDF    },
+//      { "output",  required_argument, NULL,       'o'        },
+      { NULL, 0, NULL, 0 }
+   };
 
    if (argc <= 1)
      {
-        ERR("Usage: %s desc_file", argv[0]);
+        ERR("Usage: %s [--pdf] desc_file", argv[0]);
         exit(1);
      }
-   buffer = _file_get_as_string(argv[1]);
+
+   for (opt = 0; (opt = getopt_long(argc, argv, "ho:", opts, NULL)) != -1; )
+     {
+        switch (opt)
+          {
+           case 0: break;
+#if 0
+           case 'o':
+                   outf = optarg;
+                   break;
+#endif
+           case 'h':
+                   help = EINA_TRUE;
+                   break;
+           default:
+                   help = EINA_TRUE;
+                   break;
+          }
+     }
+
+   if (help)
+     {
+        printf("Usage: %s [-h/--help] [--pdf] [--output/-o outfile] desc_file ... \n", argv[0]);
+        printf("       --help/-h Print that help\n");
+        printf("       --pdf Generate a PDF output\n");
+        goto end;
+     }
+   char *desc_file = argv[optind++];
+   buffer = _file_get_as_string(desc_file);
    if (!buffer) exit(1);
    Year_Desc *ydesc = desc_parse(buffer);
    free(buffer);
 
-   dir = dirname(argv[1]);
+   dir = dirname(desc_file);
    for (m = 0; m < 12; m++)
      {
         sprintf(history_file, "%s/History_%d_%.2d.txt", dir, ydesc->year, m + 1);
@@ -83,6 +159,11 @@ elm_main(int argc, char **argv)
           }
      }
 
+   if (gen_what == GEN_PDF)
+     {
+        _pdf_generate(ydesc, "toto.tex");
+        goto end;
+     }
    Eo *win = elm_win_util_standard_add("Accounting", "Accounting");
    evas_object_smart_callback_add(win, "delete,request", _my_win_del, NULL);
    evas_object_resize(win, 1200, 768);
@@ -94,6 +175,7 @@ elm_main(int argc, char **argv)
 
    elm_run();
 
+end:
    eina_shutdown();
    return 0;
 }
